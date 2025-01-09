73ce61474fb05d29f9620187d818a905a0ff64cb
av2215 <av2215@bs.amalnet.k12.il>
2025-01-09 18:17:41 +0200

    matala1

--- app/src/main/java/com/example/ex06031/MainActivity.java
+++ app/src/main/java/com/example/ex06031/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex06031;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +12,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button Clickme;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Clickme=findViewById(R.id.Clickme);
+        tv=findViewById(R.id.tv);
+
+    }
+
+    public void go(View view) {
+        tv.setText("Oh, yea, I’ve been clicked!");
+        tv.setTextSize(23);
+
     }
 }
